16cb85fb0b93f50e550f44f53b7bcd1bb4e30485
Anny Kao <railgun81025@gmail.com>
2021-01-08 00:57:33 +0800

    🚧　login page
    新增login相關功能頁面(加密問題還沒解決)

---
+++ public/js/login.js
@@ -0,0 +1,22 @@
+document.write('<script src=" https://code.jquery.com/jquery-3.5.1.min.js"></script>');
+function login() {
+    var _account = $('#email').val();
+    var _password = $('#password').val();
+    console.log("login");
+    if (!_account || !_password) {
+        $('#errmsg').text('請輸入帳號密碼!');
+    }
+    else {
+        $.post("/memberApi/login", { 'account': _account, 'password': _password }, 
+        function (res) {
+            if (res.status == 1) {
+                $('#errmsg').text(res.msg);
+            } else {
+                $.cookie('userName', res.data.name);
+                $.cookie('userID', res.data.account);
+                alert('登入成功!');
+                location.href = '/public/index.html';
+            }
+        });
+    }
+}
