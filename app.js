7ac99d849ed7cdb6b77bedcffee729896a25180c
Anny Kao <railgun81025@gmail.com>
2021-01-09 00:32:27 +0800

    🩹 fix way
    修正修改檔名未改到的連結

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const memberRouter=require('./routes/memberApi');
+const memberRouter=require('./routes/member-api');
 
 var app = express();
 
