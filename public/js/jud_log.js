0fe3ad398498eb05200fe2b9d2d0099c4bd661fc
Iotalh <marceline.cheng+bt@gmail.com>
2021-01-11 00:06:15 +0800

    🗑 Remove login check functions

--- public/js/jud_log.js
+++ public/js/jud_log.js
@@ -1,65 +1,22 @@
 //判斷是否登入，每個html皆需引入
-
-// document.write('<script src=" https://code.jquery.com/jquery-3.5.1.min.js"></script>');
-// document.write('<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>');
-// function checkLoginAndAlert() {
-//     if ($.cookie('userName') == null) {
-//         const login_content =
-//             `<div class="modal fade show pr-4 d-block" id="addFav" tabindex="-1" aria-labelledby="addFav" aria-modal="true" role="dialog">
-//                 <div class="modal-dialog modal-dialog-centered">
-//                     <div class="modal-content">
-//                         <div class="modal-body text-center text-primary">請先登入</div>
-//                     </div>
-//                 </div>
-//             </div>`;
-//         $('body').append(login_content);
-//         setTimeout(" location.href = '/public/login.html'", 500);
-//         return false;
-//     }
-//     else {
-//         return true;
-//     }
-// }
-
-// function goToFavList() {
-//     if (checkLoginAndAlert()) {
-//         location.href = '/public/fav-list.html';
-//     }
-// }
-
-// function addFav() {
-//     if (checkLoginAndAlert()) {
-//         const addFav_content =
-//             `<div class="modal fade show pr-4 d-block" id="addFav" tabindex="-1" aria-labelledby="addFav" aria-modal="true" role="dialog">
-//                 <div class="modal-dialog modal-dialog-centered">
-//                     <div class="modal-content">
-//                         <div class="modal-body text-center text-primary">已加入我的收藏</div>
-//                     </div>
-//                 </div>
-// 	        </div>`;
-//         $('body').append(addFav_content);
-//         $('#addFav').click(function () {
-//             $("#addFav").remove();
-//         })
-//     }
-// }
-
-
 $(document).ready(function () {
     if ($.cookie('userName') == null) {
         //尚未登入
-        document.getElementById("login_text").innerHTML = '<i class="fas fa-user-alt"></i> ' + ' ' + 'log in';
-        $('#login_text').attr('href', 'login.html');
+        document.getElementById("loginText").innerHTML = '<i class="fas fa-user-alt"></i> Log in';
+        $('#loginText').attr('href', 'login.html');
     }
     else {
         //已登入
-        document.getElementById("login_text").innerHTML = '<i class="fas fa-user-alt"></i> ' + ' ' + $.cookie('userName');
-        $('#login,#menu').mouseover(function () {
-            $('#menu').show();
-        });
-        $('#login,#menu').mouseout(function () {
-            $('#menu').hide();
-        });
+        document.getElementById("loginText").innerHTML = '<i class="fas fa-user-alt"></i> ' + $.cookie('userName');
+        const dropdown = 
+        `<div class="dropdown-menu position-absolute" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item h5 font-weight-normal text-secondary-dark"
+            href="mypage.html">會員中心</a>
+        <a class="dropdown-item h5 font-weight-normal text-secondary-dark" href="#"
+            id="logout">登出</a>
+    </div>`;
+        $("#login").addClass("dropdown");
+        $("#login").append(dropdown);
     }
     $('#logout').click(function () {
         $.removeCookie("userName");
