cdbd1ec14f2736b8a1d542413d0505406b979a22
Max <user@gmail.com>
2021-01-11 12:39:04 +0800

    🚧 fix change-pw

--- public/js/change-pw.js
+++ public/js/change-pw.js
@@ -14,9 +14,23 @@ function changePW() {
         </div>`;
         
         $('body').append(login_content);
-        setTimeout(" location.href = '../mypage.html'", 500);
+        setTimeout(" location.href = '../mypage.html'", 800);
        
-    } else {
+    } 
+    else if(_newPW != _newPW_check){
+        const login_content =
+        `<div class="modal fade show pr-4 d-block" id="addFav" tabindex="-1" aria-labelledby="addFav" aria-modal="true" role="dialog">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-body text-center text-primary">新密碼與確認密碼不相同</div>
+                </div>
+            </div>
+        </div>`;
+        
+        $('body').append(login_content);
+        setTimeout(" location.href = '../mypage.html'", 800);
+    }
+    else {
         const api = "/memberApi/changePW";
         var data = {'account': $.cookie('userAccount'), 'oldpassword': _oldPW, 'newpassword':_newPW};
         $.post(api, data, function (res) {
@@ -31,14 +45,14 @@ function changePW() {
             if (res.status == 1) {
                 //失敗
                 $('body').append(login_content);
-                setTimeout(" location.href = '../mypage.html'", 500);
+                setTimeout(" location.href = '../mypage.html'", 800);
             } else {
                 
                 $('body').append(login_content);
                 $.removeCookie("userName");
                 $.removeCookie("userAccount");
                 $.removeCookie("userRole");
-                setTimeout(" location.href = '../login.html'", 500);
+                setTimeout(" location.href = '../login.html'", 800);
             }
          });
     }
