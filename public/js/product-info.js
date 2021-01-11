cc10813749f074f5a8134cdc037a630973bc847c
Iotalh <marceline.cheng+bt@gmail.com>
2021-01-12 03:09:00 +0800

    🎨 Format code

--- public/js/product-info.js
+++ public/js/product-info.js
@@ -1,25 +1,24 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
     readProductInfo();
-    function readProductInfo(){
+    function readProductInfo() {
         const Url = location.href.split('?');
         const _id = Url[1].split('=');
         const api = '/productApi/findByID';
-        var data={'id':_id[1]}
-        $.post(api , data, function (res) {
+        var data = { 'id': _id[1] }
+        $.post(api, data, function (res) {
             $("#productName").text(res.data.productName);
             $('#productDetail').text(res.data.productDetail);
             $('#productPrice').text('NT$' + res.data.productPrice);
             $('#productImg').attr('src', res.data.Img_url);
-            if(sessionStorage.getItem('userRole')== "admin"){
-
-                let edit=
-                `<a href="../edit-product.html?${_id}"><i class="fas fa-pen text-primary"></i></a>`;
+            if (sessionStorage.getItem('userRole') == "admin") {
+                let edit =
+                    `<a href="../edit-product.html?${_id}"><i class="fas fa-pen text-primary"></i></a>`;
                 $('#FavAndEdit').append(edit);
             }
-            else{
-                let addFav=
-                `<i class="fas fa-heart text-primary" onclick="addFav_info()"></i>`;
+            else {
+                let addFav =
+                    `<i class="fas fa-heart text-primary" onclick="addFav_info()"></i>`;
                 $('#FavAndEdit').append(addFav);
 
             }
