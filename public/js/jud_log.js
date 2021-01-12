b7fdf1052f30c5eaeffce58d75a1e9e8262e5cc5
Iotalh <marceline.cheng+bt@gmail.com>
2021-01-12 10:23:08 +0800

    💄 Fix mobile login icon

--- public/js/jud_log.js
+++ public/js/jud_log.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
     if (sessionStorage.getItem('userName') == null) {
         //尚未登入
+        $("#m-login").attr(href, login.html);
     }
     else {
         //已登入
+        $("#m-login").removeAttr(href);
         $("#login-item").removeClass("d-none");
         $("#not-login").addClass("d-none");
         document.getElementById("login-text").innerHTML = '<i class="fas fa-user-alt"></i> ' + sessionStorage.getItem('userName');
-        const dropdown = 
-        `<div class="dropdown-menu position-absolute" aria-labelledby="dropdownMenuButton">
+        const dropdown =
+            `<div class="dropdown-menu position-absolute" aria-labelledby="dropdownMenuButton">
         <a class="dropdown-item h5 font-weight-normal text-secondary-dark"
             href="mypage.html">會員中心</a>
         <a class="dropdown-item h5 font-weight-normal text-secondary-dark" id="logout">登出</a>
@@ -20,5 +22,11 @@ $(document).ready(function () {
         sessionStorage.clear();
         location.href = "../index.html";
     });
+    $("#m-login").removeAttr(href);
+    $("#m-login").click(function () {
+        sessionStorage.clear();
+        location.href = "../index.html";
+    });
+
 });
 
